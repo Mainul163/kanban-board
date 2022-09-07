831397a12e5393c0662116217ac3e31f17b0113d
mainul163 <mainulisraf163@gmail.com>
2022-09-07 12:19:52 +0600

    done again

--- src/components/newBoard.jsx
+++ src/components/newBoard.jsx
@@ -67,12 +67,12 @@ const NewBoard = () => {
     }
   };
 
-  const onNavClick = (event, card, parentCategory) => {};
+  const onNavClick = (event, card, parentCategory) => { };
 
   const submission = async (e) => {
     e.preventDefault();
 
-  
+
     let maxNumber = 45563000;
     let randomNumber = Math.floor(Math.random() * maxNumber + 1);
 
@@ -115,7 +115,7 @@ const NewBoard = () => {
     };
     readData();
   }, [inputText]);
-  console.log(pending.length,inProgress,completed);
+ 
   return (
     <div style={{ marginTop: "40px" }}>
       <div style={{ textAlign: "center" }}>
@@ -138,44 +138,44 @@ const NewBoard = () => {
         </form>
       </div>
       {
-        pending.length === 0 && inProgress.length === 0 && completed.length === 0 ? 
-     <div style={{marginTop:"180px"}}> <HashLoader color={color} loading={loading} cssOverride={override} size={150} /> <br /> <h1 style={{textAlign:"center",color:"#36d7b7"}}>Loading....</h1></div>:<>
-      
-      <div className="board">
-        <Column
-          onHandleDrop={onHandleDrop}
-          onNavClick={onNavClick}
-          orientation={"left"}
-          cards={pending}
-          onClick={() => this.onClick(order[0])}
-          onCardBlur={onCardBlur}
-          cardHeader={"dark"}
-          name={order[0]}
-        ></Column>
-        <Column
-          onHandleDrop={onHandleDrop}
-          onNavClick={onNavClick}
-          cards={inProgress}
-          onClick={() => this.onClick(order[1])}
-          onCardBlur={onCardBlur}
-          cardHeader={"dark"}
-          name={order[1]}
-        ></Column>
-        <Column
-          onHandleDrop={onHandleDrop}
-          onNavClick={onNavClick}
-          cards={completed}
-          onClick={() => this.onClick(order[2])}
-          onCardBlur={onCardBlur}
-          cardHeader={"dark"}
-          name={order[2]}
-        ></Column>
-      </div>
-      
-      
-      </>
+        pending.length === 0 && inProgress.length === 0 && completed.length === 0 ?
+          <div style={{ marginTop: "180px" }}> <HashLoader color={color} loading={loading} cssOverride={override} size={150} /> <br /> <h1 style={{ textAlign: "center", color: "#36d7b7" }}>Loading....</h1></div> : <>
+
+            <div className="board">
+              <Column
+                onHandleDrop={onHandleDrop}
+                onNavClick={onNavClick}
+                orientation={"left"}
+                cards={pending}
+                onClick={() => this.onClick(order[0])}
+                onCardBlur={onCardBlur}
+                cardHeader={"dark"}
+                name={order[0]}
+              ></Column>
+              <Column
+                onHandleDrop={onHandleDrop}
+                onNavClick={onNavClick}
+                cards={inProgress}
+                onClick={() => this.onClick(order[1])}
+                onCardBlur={onCardBlur}
+                cardHeader={"dark"}
+                name={order[1]}
+              ></Column>
+              <Column
+                onHandleDrop={onHandleDrop}
+                onNavClick={onNavClick}
+                cards={completed}
+                onClick={() => this.onClick(order[2])}
+                onCardBlur={onCardBlur}
+                cardHeader={"dark"}
+                name={order[2]}
+              ></Column>
+            </div>
+
+
+          </>
       }
-    
+
     </div>
   );
 };
